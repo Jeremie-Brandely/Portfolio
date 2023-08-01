201fde28bea6eb01681f3351d2c5d3701172f608
Jeremie Brandely <jeremie.brandely@gmail.com>
2023-08-01 15:31:37 +0200

    Dernieres modif nav et cv

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
         <div id="navigation">
 			<a className="link-a" href="#ancre-a">A propos</a>
 			<a className="link-s" href="#ancre-s">Compétences</a> 
-			<a className="link-t" href="#ancre-t">Mes Travaux</a>
+			<a className="link-t" href="#ancre-t">Mes Projets</a>
 			<a className="link-c" href="#ancre-c">Contact</a> 
 		</div>
 		
