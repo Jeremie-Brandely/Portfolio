f76fc2ce0b9a1ff4db225572623d157da619027e
Jeremie Brandely <jeremie.brandely@gmail.com>
2023-10-16 12:24:14 +0200

    Correction orthographe

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ const About = () => {
 
             <p> Après plusieurs années passées dans la vente et le commerce, j'ai décidé de m'orienter vers le métier de developpeur web. 
                 J'ai toujours été attiré par les métiers informatiques, le developpement a toujours été un objectif. 
-                J'ai donc suivi la formation dev webv d'openclassroom afin de travailler dans un univers plaisant, a la recherche de nouvelles compétences et d'adaptation. 
+                J'ai donc suivi la formation dev webv d'openclassroom afin de travailler dans un univers plaisant, à la recherche de nouvelles compétences et d'adaptation. 
             </p>
             <div className="cv">
             <a href="jeremie_brandely_CV.pdf" download="jeremie_brandely_CV.pdf">Télécharger mon CV</a>
